c9e4b7aef74b0630c7447c97e60839c21d6d0193
Mohd Ajlal <ajlal00786@gmail.com>
2025-01-13 10:50:03 +0530

    search bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import SliderToggle from "./components/SliderToggle";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   const [selected, setSelected] = useState("light");
@@ -8,9 +9,16 @@ function App() {
     document.body.className = selected;
   }, [selected]);
 
+  const handleSearch = (query) => {
+    console.log('Searching for:', query);
+  };
+
   return (
     <div className="h-screen grid place-items-center px-4">
-      <div className="fixed top-4 right-4 sm:top-6 sm:right-6 lg:top-8 lg:right-8">
+      <div className="fixed top-4 right-4 sm:top-6 sm:right-6 lg:top-8 lg:right-8 space-y-4 inline-flex gap-5">
+        
+        <SearchBar onSearch={handleSearch} />
+        
         <SliderToggle selected={selected} setSelected={setSelected} />
       </div>
 
